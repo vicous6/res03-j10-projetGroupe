0c88a3b38445625f527958376596965b3848656f
vicous6 <vicous@free.fr>
2023-01-17 08:31:59 +0000

    erqfrez

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
    let scene1 = new Scene(1,"intro","preseqesvcevntation","question 1","tet bommmmm",3,"text2",4)
     let scene2 = new Scene(2,"inteqrcqesdcro","presentation","question 2","et voilaaaaa",5,"text2",6)
   
-//   let scene2 =new SceneTest()
+
 
     data.push(scene0);
      data.push(scene1);
       data.push(scene2);
-    console.log(data)
+      
+      
+    console.log(data);
 //   console.log(scene0)
 //   scene2.test()
 
-  data[0].displayInHtml()
 
+    if(window.localStorage.getItem("Current Index")!== null){
+       data[window.localStorage.getItem("Current Index")].displayInHtml(); 
+    }else{
+        data[0].displayInHtml();
+    }
      
   let button = document.getElementById("button")
   
@@ -39,15 +45,18 @@ button.addEventListener("click", function(event){
  
           let input0 = document.getElementById("chce1")
         let input1 = document.getElementById("chce2")
-    console.log(input0.checked,input1.checked)
     
     if(input0.checked=== true){
+      window.localStorage.setItem("Current Index",   input0.value);
+       console.log('let go to object' + input0.value)
       data[input0.value].displayInHtml()
-      console.log("ok")
+     
     }else 
     if(input1.checked=== true){
+            window.localStorage.setItem("Current Index",   input1.value);
+            console.log('let go to object' + input1.value)
        data[input1.value].displayInHtml() 
-        console.log("ok1")
+     
     }
     
     
