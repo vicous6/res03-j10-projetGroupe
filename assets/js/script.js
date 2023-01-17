e3cab06ac8c5a3677c451b0d152fb1dccb8e63bf
vicous6 <vicous@free.fr>
2023-01-17 11:39:47 +0000

    commit de midi

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 //   let form = document.getElementsByTagName("form")
   let data = []
-  
-  let scene0 = new Scene(0,"Benvenue dans cette nouvelle aventure (tin tin tin ) ","assets/src/images/1000_F_59827834_i4245GImXiV5Bde6RFJEPNXMFvRNKH2t.jpg","Choisissez quel personnage vous souhaiterez incarner","Le Père Noel",1,"Le Mère Noel",2)
-   let scene1 = new Scene(1,"deuxièeme introoooo","assets/src/images/1000_F_63923978_s2mPCruPpYawQfHHr1B4nB2sIAFackIK.jpg","question 1","tet bommmmm",3,"text2",4)
-    let scene2 = new Scene(2,"inteqrcqesdcro","assets/src/images/1000_F_323861330_JTzrw3SEVhzyTt19K942of6i5mbySSHc.jpg","question 2","et voilaaaaa",5,"text2",6)
-  
-
+  let scene0 = new Scene(0, "Vous incarnerai Père Noël, vous savez reçu un mail de la mere Joël qui vous dis qu\'il y a un espion dans votre atelier. Armé de votre BfG (Big Fucking Gun) vous courrez à l\'atelier malgré l\'heure tardive", "assets/src/images/1000_F_59827834_i4245GImXiV5Bde6RFJEPNXMFvRNKH2t.jpg", " 2 possibilités s'offre à vous:", "Vous allez au toilette car il est l'heure de la grosse commission", 1, "Vous entrez dans votre atelier et vous cachez dans un endroit sombre pour ne pas être vu.", 2)
+  let scene1 = new Scene(1, "", "assets/src/images/1000_F_63923978_s2mPCruPpYawQfHHr1B4nB2sIAFackIK.jpg", "Vous courrez à toute allure mais Joséphine ange gardien vous bloque la route", "Vous sortez le BFG.", 3, " Vous lui dite de faire attention à la peau de banane derriere elle.", 4)
+  let scene2 = new Scene(2, "Vous voyez un de vos petit lutin qui essai de se faire discret", "assets/src/images/1000_F_323861330_JTzrw3SEVhzyTt19K942of6i5mbySSHc.jpg", "Que faites vous?", "Vous l'attrapez par le chapeau", 5, " Vous lui demandait se qu'il fait içi à cette heure si tardive", 6)
+  let scene3 = new Scene(2, "le temps de sortir votre BFG Joséphine claque des doigts tel un thanos... vous partez en poussière.l'histoire raconte qu'elle a été embauché par ubisoft qui voulait savoir comment vous faite pour finir vos jeux à temps.", "assets/src/images/1000_F_323861330_JTzrw3SEVhzyTt19K942of6i5mbySSHc.jpg", "Vous etes mort", "Recommencer une aventure", 0, "Recommencer", 0)
+  let  scene4 = new Scene(2,"le temps qu'elle se retourne vous l'attrapez et vous la menacée avec votre BFG. Elle vous raconte qu'elle a été embauché par ubisoft pour vous espionner. connaitre votre secret pour savoir comment finir des jeux à temps. Vous finissez par la dégomer.","assets/src/images/1000_F_323861330_JTzrw3SEVhzyTt19K942of6i5mbySSHc.jpg","Bravooo","Recommencer une aventure",0,"Recommencer",0)
 
     data.push(scene0);
      data.push(scene1);
       data.push(scene2);
+    data.push(scene3);
+        data.push(scene4);
       
       
     console.log(data);
